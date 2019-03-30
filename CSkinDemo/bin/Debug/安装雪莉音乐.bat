ba0b4ee4752d8acf71d7922ab3d22cbe4f3acd18
Sherry <i@sherry.cf>
2019-03-30 19:04:29 +0800

    Add files via upload
    安装程序(其实只是发送快捷方式)

---
+++ CSkinDemo/bin/Debug/安装雪莉音乐.bat
@@ -0,0 +1,28 @@
+@echo off
+::设置程序或文件的完整路径（必选）
+set Program=%~dp0\雪莉音乐.exe
+ 
+::设置快捷方式名称（必选）
+set LnkName=雪莉音乐
+ 
+::设置快捷方式显示的说明（可选）
+set Desc=多个平台，一次畅听
+ 
+if not defined WorkDir call:GetWorkDir "%Program%"
+(echo Set WshShell=CreateObject("WScript.Shell"^)
+echo strDesKtop=WshShell.SpecialFolders("DesKtop"^)
+echo Set oShellLink=WshShell.CreateShortcut(strDesKtop^&"\%LnkName%.lnk"^)
+echo oShellLink.TargetPath="%Program%"
+echo oShellLink.WorkingDirectory="%WorkDir%"
+echo oShellLink.WindowStyle=1
+echo oShellLink.Description="%Desc%"
+echo oShellLink.Save)>makelnk.vbs
+echo 桌面快捷方式创建成功！
+makelnk.vbs
+del /f /q makelnk.vbs
+exit
+goto :eof
+:GetWorkDir
+set WorkDir=%~dp1
+set WorkDir=%WorkDir:~,-1%
+goto :eof
